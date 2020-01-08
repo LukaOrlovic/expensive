aad42e18b4d6ed8b0b23bb6053928188f5f0d763
Luka Orlovic <orlovicluka@gmail.com>
2020-01-08 20:25:04 +0100

    Implemented sql inserting values into users and receipt tables

---
+++ src/migrations/00000000000110-users.up.sql
@@ -0,0 +1,5 @@
+insert into users (username, email, password) values
+('LukaOrlovic', 'orlovic@mail.com', '$2a$11$y.BIhRIo0Z4a2U7Zb0i0a.TUZGlDM0VUctAaBJOq0LUxxIGx8QFn.'),
+('JovanaPajcin', 'pajcin@mail.com', '$2a$11$gjM5CBhYolLWaXl9jWXvW.zB77J/M6kIWMMzqor4fS6PKC.YVpVfy'),
+('PavleOrlovic', 'orlovic1@mail.com', '$2a$11$98gGNSzcjNRW6jyf/LAsQOo/0QPZ7liriagdlzxcuprvj8YDrEgPS'),
+('NikolaVujic', 'vujic@mail.com', '$2a$11$9ir4LOOHs35ltjkiVNbXde.rzfDrVCVC7k/x26C.5ThrX2ptrMcl2');
